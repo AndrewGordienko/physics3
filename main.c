3f9fe2c841e5f3d717a8708f581af28fd726e947
Andrew <andrew.gordienko05@gmail.com>
2023-06-05 00:03:36 -0400

    h

--- main.c
+++ main.c
@@ -10,20 +10,7 @@
 #define SCREEN_HEIGHT 600
 #define ORBIT_RADIUS 200.0
 #define ORBIT_SPEED 0.01
-<<<<<<< HEAD
 #define GRAVITY_STRENGTH 1
-=======
-#define GRAVITY_STRENGTH 0.5
-
-typedef struct {
-    double angle;
-    double initialX;
-    double initialY;
-    double distanceToCenter;
-    double radius;
-    int isAttracted;  // Flag to indicate if the body is attracted to the center
-} Shape;
->>>>>>> 1560941e3d478972062cbebf5b4f85964d4d34e5
 
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -54,7 +41,6 @@ int main(int argc, char* argv[]) {
     int centerX = SCREEN_WIDTH / 2;
     int centerY = SCREEN_HEIGHT / 2;
 
-<<<<<<< HEAD
     const int NUM_ORBITING_BODIES = 10;
     OrbitingBody orbitingBodies[NUM_ORBITING_BODIES];
     for (int i = 0; i < NUM_ORBITING_BODIES; ++i) {
@@ -66,15 +52,6 @@ int main(int argc, char* argv[]) {
         body.initialY = ((double)rand() / (double)RAND_MAX) * (SCREEN_HEIGHT - 200) + 100;
 
         body.distanceToCenter = sqrt(pow(body.initialX - centerX, 2) + pow(body.initialY - centerY, 2));
-=======
-    const int NUM_SHAPES = 10;
-    Shape shapes[NUM_SHAPES];
-    for (int i = 0; i < NUM_SHAPES; ++i) {
-        Shape shape;
-        shape.angle = ((double)rand() / (double)RAND_MAX) * 2.0 * M_PI;
-        shape.radius = 20.0;
-        shape.isAttracted = rand() % 2;  // Randomly set if the body is attracted to the center
->>>>>>> 1560941e3d478972062cbebf5b4f85964d4d34e5
 
         orbitingBodies[i] = body;
     }
@@ -108,26 +85,10 @@ int main(int argc, char* argv[]) {
             orbiting_body_draw(body, renderer);
         }
 
-<<<<<<< HEAD
         for (int i = 0; i < NUM_GRAVITATING_BODIES; ++i) {
             GravitatingBody* body = &gravitatingBodies[i];
             gravitating_body_step(body, centerX, centerY, GRAVITY_STRENGTH);
             gravitating_body_draw(body, renderer, centerX, centerY, GRAVITY_STRENGTH);
-=======
-            if (shape->isAttracted) {
-                double dx = centerX - shape->initialX;
-                double dy = centerY - shape->initialY;
-                double distance = sqrt(dx * dx + dy * dy);
-                double directionX = dx / distance;
-                double directionY = dy / distance;
-
-                shape->initialX += directionX * GRAVITY_STRENGTH * distance;
-                shape->initialY += directionY * GRAVITY_STRENGTH * distance;
-            }
-
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            filledCircleColor(renderer, (int)shape->initialX, (int)shape->initialY, (int)shape->radius, 0xFFFFFFFF);
->>>>>>> 1560941e3d478972062cbebf5b4f85964d4d34e5
         }
 
         SDL_RenderPresent(renderer);
